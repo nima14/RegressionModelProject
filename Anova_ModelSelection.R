c044b9734d1bc91a212a7d637dcd20c3b2aa913f
Nima <Nimataghidoust@gmail.com>
2020-04-23 22:33:36 +0430

    Anova changed

--- Anova_ModelSelection.R
+++ Anova_ModelSelection.R
@@ -19,6 +19,8 @@ RSS1 <- sum( resid(Model1)^2 )
 
 RSS2 <- sum( resid(Model2)^2 )
 
+((RSS1 - RSS2)/(2))/(RSS2/(n-4))
+
 SS <- RSS1-RSS2
 
 RSq1 <- summary(Model1)$r.square
@@ -30,7 +32,18 @@ Constraints <- AnovaRes[1,1] - AnovaRes[2,1]  #Differnece of DF between Model2 &
 
 UnConstrained <- 4 #Number of variables in Model2
 
-F <- (RSq2-RSq1)* (n - UnConstrained - 1) /  ((1 - RSq2) * Constraints) 
+RS21 <- (RSq2-RSq1)/Constraints
+
+RS2 <- (1 - RSq2)/(n - UnConstrained - 1)
+
+F <- RS21 / RS2
+
+# This F is dervied from Sum of Squares instead of R.Sq
+RS21B <- ((RSS1 - RSS2)/(Constraints))
+RS2B <- RSS2/(n-UnConstrained-1)
+
+F2 <- RS21B/RS2B
+
 
 P_Value <- pf(F,Constraints,(n - UnConstrained - 1),lower.tail = FALSE)
 
