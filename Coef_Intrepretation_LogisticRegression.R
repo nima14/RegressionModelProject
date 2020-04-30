759bf568913ed5560294ad4dbcf8c61dac62eafb
Nima <Nimataghidoust@gmail.com>
2020-05-01 01:57:09 +0430

    Another example added to Logistic Regression

--- Coef_Intrepretation_LogisticRegression.R
+++ Coef_Intrepretation_LogisticRegression.R
@@ -10,3 +10,24 @@ ChangeInOdds <- odds[2] / odds[1]
 
 ExpCoef <- exp(0.7)
 
+
+#-----------------------------------------------
+
+
+x <- c(1:10)
+
+y <- c(rep(0,5),rep(1,5))
+
+fit <- glm(y~x,family="binomial")
+
+#how many times does odds increase fot 1 increase in x?
+exp(summary(fit)$coef)
+
+plot(x,y)
+abline(glm(y~x,family="binomial"))
+
+
+lnodds <- predict(fit)
+
+exp(lnodds)/(1+exp(lnodds))
+
